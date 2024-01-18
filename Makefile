d5466540f738ed960394a50e14e4273b4e6f5fa2
micronoyau <leo.lf.dk@gmail.com>
2024-01-18 19:43:17 +0100

    Testing if A20 line is enabled in bootloader

--- Makefile
+++ Makefile
@@ -1,10 +1,19 @@
+OUT="boot.bin"
+FILENAME="boot.s"
+
 all: build run
 
 build:
-	nasm -f bin boot.s -o boot.bin
+	nasm -f bin $(FILENAME) -o $(OUT)
 
 run: build
-	qemu-system-x86_64 -drive file=boot.bin,format=raw
+	qemu-system-x86_64 -drive file=$(OUT),format=raw
 
 debug: build
-	qemu-system-x86_64 -drive file=boot.bin,format=raw -S -gdb tcp::1234
+	qemu-system-i386 -drive file=$(OUT),format=raw -S -gdb tcp::1234 &
+	gdb $(OUT) \
+		-ex "target remote localhost:1234" \
+		-ex "set architecture i8086" \
+		-ex "b *0x7c00" \
+		-ex "c" \
+
--- boot.s
+++ boot.s
@@ -1,59 +1,109 @@
-org 0x7c00  ; The BIOS has its own IVT and stuff it needs. So the bootcode
-            ; is loaded at address 0x7c00 to make room for BIOS's own memory
+org 0x7c00  ; The BIOS has its own IVT and stuff it needs. So the bootcode is loaded at address 0x7c00 to make room for BIOS's own memory
 
 section .boot
-    mov ax, 0
-    mov es, ax      ; Initialize es to 0
+    ; Set extra segment register to zero
+    xor ax, ax
+    mov es, ax
 
-    ; For BIOS interrupts see http://www.ctyme.com/intr/int-10.htm
-    ; or https://en.wikipedia.org/wiki/INT_10H
+    ; Set data segment register to 0xffff
+    not ax
+    mov ds, ax
 
-    ; Clear screen
-    mov ah, 0x07    ; Scroll down window
-    mov al, 0       ; Entire window
-    mov bh, 0x0f    ; Attributes (colour)
-    xor cx, cx      ; Row | Column of top left corner
-    mov dx, 0x2050  ; Row | Column of bottom right corner (arbitrary here)
-    int 0x10
+    ; Output start status
+    mov ax, str_welcome
+    call print
 
-    ; Display string
-    mov ah, 0x13    ; Display string
-    mov al, 1       ; Update cursor after writing
-    mov bh, 0       ; Page number
-    mov bl, 0x0f    ; Attributes (color)
-    mov cx, 13      ; Length of string
-    mov dx, 0       ; Row | Column
-    mov bp, string  ; String to display
-    int 0x10
+    ; Wait roughly 1 second to see welcome screen
+    mov ah, 0x86
+    mov cx, 0x10
+    xor dx, dx
+    int 0x15
 
-    ; Try out colors
-    mov ah, 0x13    ; Display string
-    mov al, 1       ; Update cursor after writing
-    mov bh, 0       ; Page number
-    xor bl, bl      ; Attributes (color)
-    mov cx, 1       ; Length of string
-    mov dx, 0x0100  ; Row | Column
-    mov bp, whitespace ; String to display
+    ; Test if A20 gate is enabled
+    mov si, 0x7c00 + 510
+    mov ax, word [es:si] ; Should fetch magic number 0xaa55
+    mov di, 0x7c00 + 0x100000 + 510 - 0xffff0
+    mov bx, word [ds:di] ; Is it still the magic number ?
 
-    color:
-        cmp bl, 0xf0
-        jae loop
-        int 0x10
-        ; Increase background color
-        shr bl, 4
-        inc bl
-        shl bl, 4
-        inc dl ; Increase column
-        jmp color
+    cmp ax, bx
+    je print_A20_disabled
+
+    mov ax, str_A20_enabled
+    call print
+    jmp loop
+
+    print_A20_disabled:
+        mov ax, str_A20_disabled
+        call print
 
     loop:
         jmp loop
 
-    whitespace:
-        db ' ', 0
+    ; Args : string (ax)
+    print:
+        push bp
+        push bx ; System V ABI : bx should be saved
 
-    string:
-        db "Hello, world!", 0 ; Null-terminated string
+        ; Set string as argument
+        mov bp, ax
+
+        ; Clear screen
+        mov ah, 0x07    ; Scroll down window
+        mov al, 0       ; Entire window
+        mov bh, 0x0f    ; Attributes (colour)
+        xor cx, cx      ; Row | Column of top left corner
+        mov dx, 0x2050  ; Row | Column of bottom right corner (arbitrary here)
+        int 0x10
+
+        ; Display string
+        ; First compute length of string
+        mov ax, bp
+        call strlen
+        mov cx, ax
+
+        mov ah, 0x13    ; Display string
+        mov al, 1       ; Update cursor after writing
+        mov bh, 0       ; Page number
+        mov bl, 0x0f    ; Attributes (color)
+        mov dx, 0       ; Row | Column
+        int 0x10
+
+        pop bx
+        pop bp
+        ret
+
+    ; Args : string (ax)
+    strlen:
+        push bp
+        push si
+
+        xor cx, cx
+        mov si, ax
+
+        strlen_loop:
+            mov dl, byte [es:si]
+            cmp dl, 0
+            jz strlen_end
+            inc cx
+            inc si
+            jmp strlen_loop
+
+        strlen_end:
+            mov ax, cx
+            pop si
+            pop bp
+            ret
+
+
+    str_welcome:
+        db "Welcome to the best bootloader ever, my friend!", 0
+
+    str_A20_enabled:
+        db "A20 line is enabled", 0
+
+    str_A20_disabled:
+        db "A20 line is disabled", 0
+
 
     times 510-($-$$) db 0x90 ; Sector is 512 bytes long
     dw 0xaa55 ; End of sector needs magic bytes 55, aa
